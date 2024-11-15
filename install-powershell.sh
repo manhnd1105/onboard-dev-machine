cb37d52144e6127fb042fd7b016589fdcf3632fe
manhnd1105 <manhnd.work@gmail.com>
2024-11-15 22:13:12 +0700

    Create install-powershell.sh

---
+++ install-powershell.sh
@@ -0,0 +1,5 @@
+wget -q https://packages.microsoft.com/config/ubuntu/$(lsb_release -rs)/packages-microsoft-prod.deb -O packages-microsoft-prod.deb
+sudo dpkg -i packages-microsoft-prod.deb
+sudo apt update
+sudo apt install powershell -y
+chsh < /usr/bin/pwsh
