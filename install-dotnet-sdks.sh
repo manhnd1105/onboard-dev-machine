4b6f670302f38f0891f0cc1a9c8109e86018dd64
manhnd1105 <manhnd.work@gmail.com>
2024-11-16 10:32:08 +0700

    Create install-dotnet-sdks.sh

---
+++ install-dotnet-sdks.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+curl -O https://download.visualstudio.microsoft.com/download/pr/35b1b4d1-b8f4-4b5c-9ddf-e64a846ee50b/93cc198f1c48fe5c4853bd937226f570/dotnet-sdk-6.0.428-linux-x64.tar.gz
+curl -O https://download.visualstudio.microsoft.com/download/pr/4e3b04aa-c015-4e06-a42e-05f9f3c54ed2/74d1bb68e330eea13ecfc47f7cf9aeb7/dotnet-sdk-8.0.404-linux-x64.tar.gz
+mkdir -p $HOME/dotnet && tar zxf dotnet-sdk-6.0.428-linux-x64.tar.gz -C $HOME/dotnet
+tar zxf dotnet-sdk-8.0.404-linux-x64.tar.gz -C $HOME/dotnet
+rm dotnet-sdk-*.tar.gz
+
+# Add to ~/.bashrc
+echo "export DOTNET_ROOT=$HOME/dotnet" >> ~/.bashrc
+echo "export PATH=$PATH:$HOME/dotnet" >> ~/.bashrc
+source ~/.bashrc
