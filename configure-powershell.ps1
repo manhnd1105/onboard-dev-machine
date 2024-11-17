1db9521dd62eb3501074ea706b41e7ae4af6a75c
manhnd1105 <manhnd.work@gmail.com>
2024-11-17 14:45:16 +0700

    Create configure-powershell.ps1

---
+++ configure-powershell.ps1
@@ -0,0 +1,11 @@
+$Content = @"
+if (Test-Path ~/.bash_profile) {
+    `$bashEnvVars = bash -c 'source ~/.bash_profile && env'
+    `$bashEnvVars -split "`n" | ForEach-Object {
+        `$key, `$value = `$_ -split '=', 2
+        if (`$key -and `$value) { [Environment]::SetEnvironmentVariable(`$key, `$value, [System.EnvironmentVariableTarget]::Process) }
+    }
+}
+"@
+
+Add-Content -Path $PROFILE.AllUsersAllHosts -Value $Content
