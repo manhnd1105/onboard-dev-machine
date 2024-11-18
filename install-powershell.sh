4d3f8db690e27d0000b5172dc0793d58b7f07240
manhnd1105 <manhnd.work@gmail.com>
2024-11-18 21:09:23 +0700

    Update install-powershell.sh

--- install-powershell.sh
+++ install-powershell.sh
@@ -1,5 +1,8 @@
+#!/bin/bash
 wget -q https://packages.microsoft.com/config/ubuntu/$(lsb_release -rs)/packages-microsoft-prod.deb -O packages-microsoft-prod.deb
-sudo dpkg -i packages-microsoft-prod.deb
-sudo apt update
-sudo apt install powershell -y
-chsh < /usr/bin/pwsh
+dpkg -i packages-microsoft-prod.deb
+apt update
+apt install powershell -y
+
+#/usr/bin/pwsh
+chsh 
