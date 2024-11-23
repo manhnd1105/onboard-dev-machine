02eb43976ed762d3d5cdef99bbf4e002d176398c
manhnd1105 <manhnd.work@gmail.com>
2024-11-23 12:48:27 +0700

    Update install-smartgit.sh

--- install-smartgit.sh
+++ install-smartgit.sh
@@ -2,9 +2,14 @@
 
 apt install gedit libgtk-3-0 xdg-utils -y
 
-mkdir /usr/share/desktop-directories
 
-curl -O https://downloads.syntevo.com/downloads/smartgit/smartgit-linux-24_1_0.tar.gz
-tar xzf smartgit*.tar.gz
+# mkdir /usr/share/desktop-directories
+# curl -O https://downloads.syntevo.com/downloads/smartgit/smartgit-linux-24_1_0.tar.gz
+# tar xzf smartgit*.tar.gz
+# ./smartgit/bin/add-menuitem.sh
 
-./smartgit/bin/add-menuitem.sh
+add-apt-repository ppa:eugenesan/ppa
+apt-get update
+apt-get install smartgit
+
+# requires hyper-v and java SRE on the Windows machine
