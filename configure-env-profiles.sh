befdade01fdd5f78fd490ffd9137b59f73df0b62
manhnd1105 <manhnd.work@gmail.com>
2024-11-23 11:30:34 +0700

    Create configure-env-profiles.sh

---
+++ configure-env-profiles.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+mkdir -p ~/profile.d
+
+cat <<EOF >> ~/.profile
+
+# Source user environment files from ~/profile.d/
+if [ -d "\$HOME/profile.d" ]; then
+  for script in "\$HOME/.user_profile.d"/*.sh; do
+    if [ -f "\$script" ]; then
+      source "\$script"
+    fi
+  done
+fi
+EOF
+
+source ~/.profile
