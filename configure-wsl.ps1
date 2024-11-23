468ac06661f18f60b88c785a560e9f92d96d1041
manhnd1105 <manhnd.work@gmail.com>
2024-11-23 09:09:18 +0700

    Update configure-wsl.ps1

--- configure-wsl.ps1
+++ configure-wsl.ps1
@@ -1,2 +1,3 @@
 # mount the disk drive K into WSL working folder to resolve UNC issues
-subst k: \\wsl.localhost\Ubuntu\home\manhnd1105
+#subst k: \\wsl.localhost\Ubuntu\home\manhnd1105
+net use K: \\wsl.localhost\Ubuntu /persistent:yes
