152067a962c15e67b623c1495038a1c65aae9c26
manhnd1105 <manhnd.work@gmail.com>
2024-11-24 17:55:14 +0700

    Update configure-env-profiles.sh

--- configure-env-profiles.sh
+++ configure-env-profiles.sh
@@ -6,7 +6,7 @@ cat <<EOF >> ~/.profile
 
 # Source user environment files from ~/profile.d/
 if [ -d "\$HOME/profile.d" ]; then
-  for script in "\$HOME/.user_profile.d"/*.sh; do
+  for script in "\$HOME/profile.d"/*.sh; do
     if [ -f "\$script" ]; then
       source "\$script"
     fi
