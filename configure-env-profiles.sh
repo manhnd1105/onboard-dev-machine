f063cac365533176799c2ebfa92ba63fe22bcbca
manhnd1105 <manhnd.work@gmail.com>
2024-11-24 21:00:18 +0700

    Update configure-env-profiles.sh

--- configure-env-profiles.sh
+++ configure-env-profiles.sh
@@ -2,19 +2,6 @@
 
 mkdir -p ~/profile.d
 
-# for all shells
-cat <<EOF >> ~/.profile
-# Source user environment files from ~/profile.d/
-if [ -d "\$HOME/profile.d" ]; then
-  for script in "\$HOME/profile.d"/*.sh; do
-    if [ -f "\$script" ]; then
-      source "\$script"
-    fi
-  done
-fi
-EOF
-
-# fix for non-login terminal such as VS Code built-in
 cat <<EOF >> ~/.bashrc
 # Source user environment files from ~/profile.d/
 if [ -d "\$HOME/profile.d" ]; then
