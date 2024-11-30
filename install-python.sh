1a2eaf4137a272492e855df97172d1096fe5e3c9
manhnd1105 <manhnd.work@gmail.com>
2024-11-30 10:00:14 +0700

    Update install-python.sh

--- install-python.sh
+++ install-python.sh
@@ -2,3 +2,10 @@
 
 # requires sudo
 apt install python3 python3-pip
+
+cat <<EOF >> ~/profile.d/python.sh
+
+if [[ ":\$PATH:" != *":\$HOME/.local/bin:"* ]]; then
+    export PATH=\$PATH:\$HOME/.local/bin
+fi
+EOF
