8b4d91adc18c4590b236a57df79aff6abfaebc4d
manhnd1105 <manhnd.work@gmail.com>
2025-03-19 09:02:30 +0700

    Update install-python.sh

--- install-python.sh
+++ install-python.sh
@@ -7,10 +7,14 @@ pyenv install 3.12
 pyenv global 3.12
 
 cat <<EOF >> ~/profile.d/python.sh
-export PYENV_ROOT="$HOME/.pyenv"
-[[ -d $PYENV_ROOT/bin ]]
+export PYENV_ROOT="\$HOME/.pyenv"
+[[ -d \$PYENV_ROOT/bin ]]
 if [[ ":\$PATH:" != *":\$PYENV_ROOT/bin:"* ]]; then
     export PATH=\$PATH:\$PYENV_ROOT/bin
 fi
-eval "$(pyenv init - bash)"
+eval "\$(pyenv init - bash)"
+EOF
+
+cat <<EOF >> ~/.bashrc
+eval "\$(pyenv virtualenv-init -)"
 EOF
