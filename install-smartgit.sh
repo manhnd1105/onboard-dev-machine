d38a5825d423a62f7d5e51d9e7006719cced65e7
manhnd1105 <manhnd.work@gmail.com>
2025-11-24 16:38:54 +0700

    Update install-smartgit.sh

--- install-smartgit.sh
+++ install-smartgit.sh
@@ -6,6 +6,6 @@ apt install gedit libgtk-3-0 xdg-utils -y
 
 
 mkdir /usr/share/desktop-directories
-curl -O https://downloads.syntevo.com/downloads/smartgit/smartgit-linux-24_1_0.tar.gz
+curl -O https://downloads.syntevo.com/downloads/smartgit/smartgit-linux-25_1_0.tar.gz
 tar xzf smartgit*.tar.gz
 ./smartgit/bin/add-menuitem.sh
