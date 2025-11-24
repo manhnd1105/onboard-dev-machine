df9c99ca286b7466edfb35a151b1687a0b7b8d92
manhnd1105 <manhnd.work@gmail.com>
2025-11-24 20:36:16 +0700

    Add -y flag to apt-get commands for automation

--- install-ibus-bamboo.sh
+++ install-ibus-bamboo.sh
@@ -3,7 +3,7 @@
 
 # sudo
 add-apt-repository ppa:bamboo-engine/ibus-bamboo
-apt-get update
-apt-get install ibus ibus-bamboo --install-recommends
+apt-get update -y
+apt-get install -y ibus ibus-bamboo --install-recommends
 ibus restart
 env DCONF_PROFILE=ibus dconf write /desktop/ibus/general/preload-engines "['BambooUs', 'Bamboo']" && gsettings set org.gnome.desktop.input-sources sources "[('xkb', 'us'), ('ibus', 'Bamboo')]"
