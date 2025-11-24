4d17bdb59695dd4b2c4d04f501e70390ad39cd38
manhnd1105 <manhnd.work@gmail.com>
2025-11-24 20:31:29 +0700

    Add script to install IBus Bamboo input method

---
+++ install-ibus-bamboo.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+# sudo
+add-apt-repository ppa:bamboo-engine/ibus-bamboo
+apt-get update
+apt-get install ibus ibus-bamboo --install-recommends
+ibus restart
+env DCONF_PROFILE=ibus dconf write /desktop/ibus/general/preload-engines "['BambooUs', 'Bamboo']" && gsettings set org.gnome.desktop.input-sources sources "[('xkb', 'us'), ('ibus', 'Bamboo')]"
