67d1a45f266c7dcb114da10549f9fb8b464b9705
manhnd1105 <manhnd.work@gmail.com>
2025-11-25 08:38:54 +0700

    Update SmartGit download URL in install script

--- install-smartgit.sh
+++ install-smartgit.sh
@@ -6,6 +6,6 @@ apt install gedit libgtk-3-0 xdg-utils -y
 
 
 mkdir /usr/share/desktop-directories
-curl -O https://downloads.syntevo.com/downloads/smartgit/smartgit-25_1_093-linux-amd64.tar.gz
+curl -O https://download.smartgit.dev/smartgit/smartgit-25_1_093-linux-amd64.tar.gz
 tar xzf smartgit*.tar.gz
 ./smartgit/bin/add-menuitem.sh
