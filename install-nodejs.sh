a50f05c3a6c0c4bbdae2ae31b3c2b3449cce80d0
manhnd1105 <manhnd.work@gmail.com>
2026-01-05 19:56:52 +0700

    Update install-nodejs.sh

--- install-nodejs.sh
+++ install-nodejs.sh
@@ -2,8 +2,8 @@
 
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.3/install.sh | bash
 
-cat <<EOF ~/profile.d/nodejs
-export NVM_DIR="$HOME/.nvm"
-[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
-[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
+cat <<EOF ~/profile.d/nodejs.sh
+export NVM_DIR="\$HOME/.nvm"
+[ -s "\$NVM_DIR/nvm.sh" ] && \. "\$NVM_DIR/nvm.sh"  # This loads nvm
+[ -s "\$NVM_DIR/bash_completion" ] && \. "\$NVM_DIR/bash_completion"  # This loads nvm bash_completion
 EOF
